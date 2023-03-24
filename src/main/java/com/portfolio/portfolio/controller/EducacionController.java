2bd7652b9f8777ee77b4c3e84d2eb63d601c8555
TommySosa <tomas.facundo.sosa@gmail.com>
2023-03-24 20:52:27 -0300

    /

--- src/main/java/com/portfolio/portfolio/controller/EducacionController.java
+++ src/main/java/com/portfolio/portfolio/controller/EducacionController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = {"http://localhost:4200", "https://tomassosa-df065.web.app"})
+@CrossOrigin(origins = {"http://localhost:4200", "https://tomassosa-df065.web.app/"})
 public class EducacionController {
     @Autowired
     EducacionService sEducacion;
--- src/main/java/com/portfolio/portfolio/controller/ExperienciaController.java
+++ src/main/java/com/portfolio/portfolio/controller/ExperienciaController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RequestMapping ("explab")
-@CrossOrigin(origins = {"http://localhost:4200", "https://tomassosa-df065.web.app"})
+@CrossOrigin(origins = {"http://localhost:4200", "https://tomassosa-df065.web.app/"})
 @RestController
 public class ExperienciaController {
     @Autowired
